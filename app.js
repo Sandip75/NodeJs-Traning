71dabc84125fa4c1b3b10bfe35f663e4f111ece5
Sandip Kumar <Sandipk@tbotechnology.com>
2020-10-16 17:23:04 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+//#region Modules
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
+//#endregion
+
+//#region MiddleLayer
+app.use(express.static("public"));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+//#endregion
+
+//#region Render Request
+app.use("/", require("./routes"));
+//#endregion
+
+//#region listening request
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server started at port ${PORT}`);
+});
+//#endregion
