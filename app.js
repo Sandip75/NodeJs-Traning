d7321cd5a7f0e4218e6fd95043d37d516ea91466
sandip75 <sk1957339@gmail.com>
2020-10-16 18:46:15 +0530

    update router

--- app.js
+++ app.js
@@ -2,14 +2,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-var cookieParser = require('cookie-parser');
+require('dotenv').config();
 //#endregion
 
 //#region MiddleLayer
 app.use(express.static("public"));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(cookieParser());
 
 app.set("view engine", "ejs");
 //#endregion
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,13 @@
-let x= 0;
+const app = module.exports = require('express')();
+
+app.get('/', (req, res) => {
+  return res.status(200).send({msg: "Hi Developer!"});
+});
+
+//app.use('/employees', require('./employees'));
+
+
+// catch all
+app.all('*', (req, res) => {
+  return res.status(404).send({msg: "Not Found"});
+});
